878ac81bc2daadaadd1cda99f3d1de313c799bc7
karunaworms <karuna@wormsconnect.com>
2025-01-17 17:31:09 +0530

    fixed type

--- src/store/create-store.tsx
+++ src/store/create-store.tsx
@@ -19,10 +19,12 @@ export const useCartStore = create<CartStore>((set) => ({
   cart: [],
   addToCart: (product: { id: any }) =>
     set((state: any) => {
-      const existingProduct = state.cart.find((item) => item.id === product.id);
+      const existingProduct = state.cart.find(
+        (item: any) => item.id === product.id
+      );
       if (existingProduct) {
         return {
-          cart: state.cart.map((item) =>
+          cart: state.cart.map((item: any) =>
             item.id === product.id
               ? { ...item, quantity: item.quantity + 1 }
               : item
@@ -36,24 +38,29 @@ export const useCartStore = create<CartStore>((set) => ({
   removeFromCart: (productId: number) =>
     set((state: any) => {
       return {
-        cart: state.cart.filter((item) => item.id !== productId),
+        cart: state.cart.filter((item: any) => item.id !== productId),
       };
     }),
 
   updateQuantity: (productId: number, quantity: number) =>
     set((state: any) => {
       if (quantity <= 0) {
-        return { cart: state.cart.filter((item) => item.id !== productId) };
+        return {
+          cart: state.cart.filter((item: any) => item.id !== productId),
+        };
       }
 
       return {
-        cart: state.cart.map((item) =>
+        cart: state.cart.map((item: any) =>
           item.id === productId ? { ...item, quantity } : item
         ),
       };
     }),
   totalProduct: (state: any) => {
-    const total = state?.cart?.reduce((sum, item) => sum + item.quantity, 0);
+    const total = state?.cart?.reduce(
+      (sum: any, item: any) => sum + item.quantity,
+      0
+    );
 
     return total || 0;
   },
