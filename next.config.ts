4ce13efdac8998bead1c229ce60599662bd927ee
karunaworms <karuna@wormsconnect.com>
2025-01-17 17:20:38 +0530

    added gh-page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/product-cart",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
